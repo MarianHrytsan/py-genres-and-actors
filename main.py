7c723acc8120820ee18e40740c65cc7d290dd652
Marian Hrytsan <gricanmaran@gmail.com>
2026-02-22 22:16:07 +0200

    Corect solution

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from db.models import Genre, Actor
 
 
 def main() -> QuerySet:
-    actor_tuple = ("George Klooney", "Kianu Reaves", "Scarlett Keegan", "Will Smith", "Jaden Smith", "Scarlett Johansson")
+    actor_list = [("George", "Klooney"), ("Kianu", "Reaves"), ("Scarlett", "Keegan"), ("Will", "Smith"), ("Jaden", "Smith"), ("Scarlett", "Johans")]
     genre_tuple = ("Western", "Action", "Dramma")
     for genre in genre_tuple:
         Genre.objects.create(name=genre)
-    for actor in actor_tuple:
-        Actor.objects.create(first_name=actor.split(" ")[0], last_name=actor.split(" ")[1])
+    for first, last in actor_list:
+        Actor.objects.create(first_name=first, last_name=last)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
     Actor.objects.filter(last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
     Genre.objects.filter(name="Action").delete()
-    Actor.objects.filter(first_name="Scarlet").delete()
-    return Genre.objects.filter(last_name="Smith").order_by("first_name")
+    Actor.objects.filter(first_name="Scarlett").delete()
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
